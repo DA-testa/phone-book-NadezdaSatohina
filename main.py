67e77906fc3f6528e15fd50b74e6898ba0f4da4b
NadezdaSatohina <124875434+NadezdaSatohina@users.noreply.github.com>
2023-04-02 08:36:36 +0000

    test1

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # python3
 
+bucket_size = 256
+buckets = [[] for i in range(bucket_size)]
+
+def _hash_func(self, i):
+    return i % bucket_size
+
+
 class Query:
     def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
-
+            
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
@@ -19,23 +26,26 @@ def process_queries(queries):
     # Keep list of all existing (i.e. not deleted yet) contacts.
     contacts = []
     for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
+        hashed = _hash_func(cur_query.number)
+        bucket = self.buckets[hashed]
+        
+        if cur_query.type == 'add':    
+            for contact in bucket:
                 if contact.number == cur_query.number:
                     contact.name = cur_query.name
                     break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            else:
+                self.buckets[hashed].append(cur_query)
+                
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
+            for j in range(len(bucket)):
+                if bucket[j].number == cur_query.number:
+                    bucket.pop(j)
                     break
+                    
         else:
             response = 'not found'
-            for contact in contacts:
+            for contact in bucket:
                 if contact.number == cur_query.number:
                     response = contact.name
                     break
@@ -44,4 +54,5 @@ def process_queries(queries):
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
+
 
